948fc6a4a91272a868fc6fe600d1728f06f4e3aa
juantobares4 <j.tobares@itecriocuarto.org.ar>
2024-06-08 18:29:03 -0300

    deleted folders

--- guia02/ejercicio01.js
+++
@@ -1,9 +0,0 @@
-/* Dado un array de números, 
-crea una función que utilice map para duplicar cada número en el array y devolver un nuevo array con los resultados. */
-
-let arrayNumbers = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
-
-let newArray = arrayNumbers.map(value => value * 2); /* Si la función solo tiene una expresión de retorno, debes omitir las llaves { } 
-(en caso de no hacerlo, nos retornará undefined) y la palabra clave return. */
-
-console.log(newArray);
--- guia02/ejercicio02.js
+++
@@ -1,8 +0,0 @@
-/* Dado un array de números, crea una función que utilice filter 
-para filtrar solo los números pares y devolver un nuevo array con esos números. */
-
-let arrayNumbers = [23, 67, 43, 95, 22, 68, 21, 18, 15, 84, 55];
-
-let newArrayPeers = arrayNumbers.filter(number => number % 2 === 0);
-
-console.log(newArrayPeers);
